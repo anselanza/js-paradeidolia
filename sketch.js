5637a8be52a438d4a6adc8d4b5e1b024918c1cde
Stephen Buchanan <stephen.buchanan@random.nu>
2017-12-20 16:12:10 +0100

    totally works using parceljs

--- sketch.js
+++ sketch.js
@@ -1,19 +1,27 @@
-// for red, green, and blue color values
-var r, g, b;
+import p5 from 'p5';
 
-function setup() {
-  createCanvas(720, 400);
-  // Pick colors randomly
-  r = random(255);
-  g = random(255);
-  b = random(255);
-}
+console.log("I'm running!");
 
-function draw() {
-  background(127);
-  // Draw a circle
-  strokeWeight(2);
-  stroke(r, g, b);
-  fill(r, g, b, 127);
-  ellipse(360, 200, 200, 200);
-}
+let myP5 = new p5( function (sketch) {
+
+  // for red, green, and blue color values
+  let r, g, b;
+
+  sketch.setup = function() {
+    console.log('setup');
+    sketch.createCanvas(720, 400);
+    // Pick colors randomly
+    r = sketch.random(255);
+    g = sketch.random(255);
+    b = sketch.random(255);
+  }
+
+  sketch.draw = function() {
+    sketch.background(127);
+    // Draw a circle
+    sketch.strokeWeight(2);
+    sketch.stroke(r, g, b);
+    sketch.fill(r, g, b, 127);
+    sketch.ellipse(360, 200, 200, 200);
+  }
+} )
